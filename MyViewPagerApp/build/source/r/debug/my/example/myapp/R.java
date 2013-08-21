125f7aefd3eb8b3c52bf6002d6bbcc0355598004
Patrick Kimber <code@pkimber.net>
2013-08-21 10:11:21 +0100

    Simple sample is working

--- MyViewPagerApp/build/source/r/debug/my/example/myapp/R.java
+++ MyViewPagerApp/build/source/r/debug/my/example/myapp/R.java
@@ -21,10 +21,15 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080003;
+        public static final int demo_collection_button=0x7f080001;
+        public static final int demo_external_activity=0x7f080002;
+        public static final int pager=0x7f080000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int fragment_section_dummy=0x7f030001;
+        public static final int fragment_section_launchpad=0x7f030002;
     }
     public static final class menu {
         public static final int main=0x7f070000;
@@ -34,7 +39,10 @@ public final class R {
          */
         public static final int action_settings=0x7f050000;
         public static final int app_name=0x7f050001;
-        public static final int hello_world=0x7f050002;
+        public static final int demo_collection=0x7f050002;
+        public static final int demo_external_activity=0x7f050003;
+        public static final int dummy_section_text=0x7f050004;
+        public static final int hello_world=0x7f050005;
     }
     public static final class style {
         /**  From: file:/home/patrick/repo/learn/android/my_view_pager/MyViewPagerApp/src/main/res/values/styles.xml 
